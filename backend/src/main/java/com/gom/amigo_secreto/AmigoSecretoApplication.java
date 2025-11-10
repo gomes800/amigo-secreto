51b0934c353f3d91c0f7730cbc1c73d21258404a
gom-trab <jgomesleite8@gmail.com>
2025-11-10 10:44:46 -0300

    initializing backend, docker-compose postgresql

---
+++ backend/src/main/java/com/gom/amigo_secreto/AmigoSecretoApplication.java
@@ -0,0 +1,13 @@
+package com.gom.amigo_secreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoSecretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoSecretoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gom/amigo_secreto/AmigoSecretoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.amigo_secreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoSecretoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
