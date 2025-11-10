0306bf39a5c78b2fcad903e777f77739d03758c5
gom-trab <jgomesleite8@gmail.com>
2025-11-10 15:34:58 -0300

    feat: create user dto, repository, service and controller

---
+++ backend/src/main/java/com/gom/amigo_secreto/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.gom.amigo_secreto.controller;
+
+import com.gom.amigo_secreto.model.dto.UserResponseDTO;
+import com.gom.amigo_secreto.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        return ResponseEntity.ok(userService.getAll(page, size));
+    }
+}
---
+++ backend/src/main/java/com/gom/amigo_secreto/service/UserService.java
@@ -0,0 +1,25 @@
+package com.gom.amigo_secreto.service;
+
+import com.gom.amigo_secreto.model.dto.UserResponseDTO;
+import com.gom.amigo_secreto.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Page<UserResponseDTO> getAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        return userRepository.findAll(pageable)
+                .map(UserResponseDTO::fromEntity);
+    }
+}
